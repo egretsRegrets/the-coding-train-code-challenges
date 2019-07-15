9098b1f4d8309b450bb4fdebb74f7b59b4d2ce57
egretsRegrets <samuel.galen.gardner@gmail.com>
2019-07-15 10:05:17 -0400

    assets in place; jumping unicorn

--- chrome-dinosaur-game/sketch.js
+++ chrome-dinosaur-game/sketch.js
@@ -1,11 +1,30 @@
+// vars
 let unicorn;
 
+// game assets
+let uImg;
+let tImg;
+let bImg;
+
+// p5 lifecycle
+function preload() {
+    uImg = loadImage('assets/unicorn.png');
+    tImg = loadImage('assets/train.png');
+    bImg = loadImage('assets/background.jpg');
+}
 function setup() {
-    createCanvas(600, 450);
+    createCanvas(800, 450);
     unicorn = new Unicorn();
 }
-
 function draw() {
-    background(220);
+    background(bImg);
     unicorn.show(); 
+    unicorn.move();
+}
+
+// methods
+function keyPressed() {
+    if (key === ' ') {
+        unicorn.jump();
+    }
 }
--- chrome-dinosaur-game/unicorn.js
+++ chrome-dinosaur-game/unicorn.js
@@ -1,10 +1,26 @@
 class Unicorn {
     constructor() {
+        this.r = 150;
         this.x = 50;
-        this.y = height - 50;
+        this.y = height - this.r;
+        this.vy = 0;
+        this.gravity = 2;
+    }
+
+    jump() {
+        // update velocity
+        this.vy = -25;
+    }
+
+    move() {
+        // update vertical position by velocity
+        this.y += this.vy;
+        this.vy += this.gravity;
+        // keep vert position within the canvas
+        this.y = constrain(this.y, 0, height - this.r);
     }
 
     show() {
-        rect(this.x, this.y, 50, 50);
+        image(uImg, this.x, this.y, this.r, this.r);
     }
 }
