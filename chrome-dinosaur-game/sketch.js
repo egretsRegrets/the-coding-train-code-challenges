4336e8a9e23f4315244b2ab8c662c82ed309dd1f
egretsRegrets <samuel.galen.gardner@gmail.com>
2019-07-15 10:50:34 -0400

    ml5 sound classifier integration

--- chrome-dinosaur-game/sketch.js
+++ chrome-dinosaur-game/sketch.js
@@ -8,8 +8,14 @@ let bImg;
 
 let trains = [];
 
+let soundClassifier;
+
 // p5 lifecycle
 function preload() {
+    soundClassifier = ml5.soundClassifier(
+        'SpeechCommands18w', 
+        { probabilityThreshold: 0.95 }
+    );
     uImg = loadImage('assets/unicorn.png');
     tImg = loadImage('assets/train.png');
     bImg = loadImage('assets/background.jpg');
@@ -17,6 +23,7 @@ function preload() {
 function setup() {
     createCanvas(800, 450);
     unicorn = new Unicorn();
+    soundClassifier.classify(gotCommand)
 }
 function draw() {
     if (random(1) < 0.003) {
@@ -39,6 +46,15 @@ function draw() {
 }
 
 // methods
+function gotCommand(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results[0].label, results[0].confidence);
+    if (results[0].label === 'up') {
+        unicorn.jump();
+    }
+}
 function keyPressed() {
     if (key === ' ') {
         unicorn.jump();
